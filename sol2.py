6c339270018c73fde45bb9c65996d640e62ce5fc
racsosabe <racso_galvan@hotmail.com>
2019-12-09 00:57:26 -0500

    Solution with MPMath

--- sol2.py
+++ sol2.py
@@ -1,19 +1,17 @@
+from mpmath import *
 import threading
 import logging
 import time
 import multiprocessing
 import concurrent.futures
-from cmath import phase, exp
-from math import log, acos, fsum
-from math import exp as expR
 
-def norm(a):
+def norm2(a):
     return a.real**2 + a.imag**2
 
 def getMin(a, b):
     prod = a * b.conjugate()
-    if prod.real >= norm(a): return abs(a)
-    if prod.real >= norm(b): return abs(b)
+    if prod.real >= norm2(a): return abs(a)
+    if prod.real >= norm2(b): return abs(b)
     return abs(prod.imag / abs(b-a))
 
 def f(x, n):
@@ -31,71 +29,74 @@ def df(x, n):
 def M(s1, s2, n):
     ans = 0
     for i in range(2,n+1):
-        ans += log(i) * log(i) * expR(log(i) * (-min(s1.real,s2.real)))
+        ans += log(mpf(i)) * log(mpf(i)) * exp(log(mpf(i)) * (-min(s1.real,s2.real)))
     return ans
 
 def isGreater(s1, s2, localM, n):
     L = getMin(f(s1, n),f(s2, n))
-    R = localM * norm(s1 - s2) / 8
-    return L > R + 1e-20
+    R = localM * norm2(s1 - s2) / 8
+    EPS = mpf('10') ** mpf('-20')
+    return L > R + EPS
 
 def enrect(z1, z2, x):
     return min(z1.real, z2.real) <= x.real <= max(z1.real, z2.real) and min(z1.imag, z2.imag) <= x.imag <= max(z1.imag, z2.imag)
 
 def varArg(s1, s2, n):
     localM = M(s1,s2,n)
-    t0 = 0
-    t = 1
+    t0 = mpf(0)
+    t = mpf(1)
     L = s1
     d = s2 - s1
-    ans = 0
-    while t0 + 1e-20 < 1:
+    ans = mpf(0)
+    EPS1 = mpf('10') ** mpf('-20')
+    EPS2 = mpf('10') ** mpf('-15')
+    EPS3 = mpf('10') ** mpf('-10')
+    while t0 + EPS1 < 1:
         R = L + t * d
         while (not isGreater(L,R,localM,n)) and t > 1e-15:
             t /= 2
             R = L + t * d
-        if t <= 1e-15:
+        if t <= EPS2:
             print("Failed")
             if s1.real == s2.real:
-                d = 1e-10 if s1.imag < s2.imag else -1e-10
-                return varArg(s1 + complex(d,0), s2 + complex(d,0), n)
+                d = EPS3 if s1.imag < s2.imag else -EPS3
+                return varArg(s1 + mpc(d,0), s2 + mpc(d,0), n)
             else:
-                d = 1e-10 if s1.real > s2.real else -1e-10
-                return varArg(s1 + complex(0,d), s2 + complex(0,d), n)
-        ans += phase(f(R, n) / f(L, n))
+                d = EPS3 if s1.real > s2.real else -EPS3
+                return varArg(s1 + mpc(0,d), s2 + mpc(0,d), n)
+        ans += arg(f(R, n) / f(L, n))
         t0 += t
         L += d * t
-        t = min(2 * t, 1 + 1e-20 - t0)
+        t = min(2 * t, 1 + EPS1 - t0)
     return ans
 
 def getRectangle(LD, RU, n):
-    LU = complex(LD.real,RU.imag)
-    RD = complex(RU.real,LD.imag)
+    LU = mpc(LD.real,RU.imag)
+    RD = mpc(RU.real,LD.imag)
     return round((varArg(LD,RD,n) + varArg(RD,RU,n) + varArg(RU,LU,n) + varArg(LU,LD,n)) / (2 * acos(-1)))
-
-def norm1(x):
-    return max(abs(x.real), abs(x.imag))
 
 def newton(LD, RU, n):
     it = 10000
-    x0 = (LD + RU) * 0.5
-    xi = x0 + complex(1,0)
-    tol = 10**(-20)
-    while norm1(x0 - xi) > tol and enrect(LD, RU, x0) and it > 0:
+    x0 = (LD + RU) * mpf('0.5')
+    xi = x0 + mpc(1,0)
+    tol = mpf('10') ** mpf('-30')
+    print("Started Newton")
+    while abs(x0 - xi) > tol and enrect(LD, RU, x0) and it > 0:
         xi = x0
         x0 = xi - f(xi, n) / df(xi, n)
         it -= 1 
-    if min(norm1(x0 - xi), norm1(f(x0, n))) <= 10**(-12) and enrect(LD,RU,x0):
+    print(x0,xi,abs(x0-xi),tol)
+    if abs(x0 - xi) <= tol and enrect(LD,RU,x0):
         return x0
-    return complex(3,0)
+    return mpc(3,0)
 
 class MutexStack:
     def __init__(self, LD, RU, _n):
         self.stack = []
         self.n = _n
         self._lock = threading.Lock()
-        LU = complex(LD.real,RU.imag)
-        RD = complex(RU.real,LD.imag)
+        LU = mpc(LD.real,RU.imag)
+        RD = mpc(RU.real,LD.imag)
         self.stack = [LD,RU,varArg(LD,RD,_n),varArg(RD,RU,_n),varArg(RU,LU,_n),varArg(LU,LD,_n)]
 
     def work(self, name):
@@ -111,15 +112,15 @@ class MutexStack:
                 V2 = self.stack[-2]
                 V3 = self.stack[-1]
                 for _ in range(6): self.stack.pop()
-                if zeros == 0: return [2, complex(3,0)]
+                if zeros == 0: return [2, mpc(3,0)]
                 if zeros == 1:
                     O = (LD + RU) / 2
                     res = newton(LD, RU, n)
                     if enrect(LD, RU, res):
                         return [1, res]
                 if abs(LD.real - RU.real) > abs(LD.imag - RU.imag):
-                    M1 = complex((LD.real + RU.real) / 2, LD.imag)
-                    M2 = complex((LD.real + RU.real) / 2, RU.imag)
+                    M1 = mpc((LD.real + RU.real) / 2, LD.imag)
+                    M2 = mpc((LD.real + RU.real) / 2, RU.imag)
                     FD = varArg(LD,M1,n)
                     FU = varArg(RU,M2,n)
                     FM = varArg(M1,M2,n)
@@ -128,18 +129,18 @@ class MutexStack:
                     for x in [M1,RU,V0 - FD,V1,FU,-FM]:
                         self.stack.append(x)
                 else:
-                    M1 = complex(RU.real, (LD.imag + RU.imag) / 2)
-                    M2 = complex(LD.real, (LD.imag + RU.imag) / 2)
-                    FD = varArg(complex(RU.real,LD.imag),M1,n)
-                    FU = varArg(complex(LD.real,RU.imag),M2,n)
+                    M1 = mpc(RU.real, (LD.imag + RU.imag) / 2)
+                    M2 = mpc(LD.real, (LD.imag + RU.imag) / 2)
+                    FD = varArg(mpc(RU.real,LD.imag),M1,n)
+                    FU = varArg(mpc(LD.real,RU.imag),M2,n)
                     FM = varArg(M1,M2,n)
                     for x in [LD,M1,V0,FD,FM,V3-FU]:
                         self.stack.append(x)
                     for x in [M2,RU,-FM,V1-FD,V2,FU]:
                         self.stack.append(x)
-                return [2, complex(3,0)]
+                return [2, mpc('3','0')]
             else:
-                return [0, complex(3,0)]
+                return [0, mpc('3','0')]
 
 
 class MutexAnswer:
@@ -160,26 +161,33 @@ class MutexAnswer:
                 self.len -= 1
                 return res
             else:
-                return complex(3,0)
+                return mpc('3','0')
 
     def size(self):
         return self.len
 
 def solve(S, R, id):
     val = S.work(id)
+    print("Working from {0:d}".format(id))
     while val[0] >= 1:
         if val[0] == 1:
             print("Found answer from {0:d}: ".format(id),val[1])
             R.push(val[1])
         val = S.work(id)
 
+n = 3
+mp.dps = 100
 threads = multiprocessing.cpu_count()
-S = MutexStack(complex(-1,0),complex(1,50000),3)
+LD = mpc('-1','0')
+RU = mpc('1','100')
+print(LD)
+print(LD,RU,n)
+S = MutexStack(LD,RU,n)
 R = MutexAnswer()
 with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
     for index in range(threads):
         executor.submit(solve, S, R, index)
-print(getRectangle(complex(-1,0),complex(1,50000),3))
+print(getRectangle(LD,RU,n))
 print("Answers:")
 print(R.size())
 res = R.pop()
