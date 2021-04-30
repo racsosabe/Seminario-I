c0a4ac97eea400e18e4bd7ad2f94128410a0dc32
racsosabe <racso_galvan@hotmail.com>
2021-04-30 13:59:01 -0500

    Added n = 6

---
+++ getUnique.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace::std;
+
+set<string> used;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	string s;
+	int cnt = 0;
+	while(getline(cin, s)){
+		if(used.count(s)) continue;
+		cnt += 1;
+		used.emplace(s);
+		cout << s << endl;
+	}
+	cerr << cnt << endl;
+	return 0;
+}
